156f84b97f19739e6be092b333fd238c3fe00c3e
edward <1483500659@qq.com>
2019-08-27 17:40:42 +0800

    aug27

--- vueApp/src/components/workComponents/returnInquire.vue
+++ vueApp/src/components/workComponents/returnInquire.vue
@@ -1,37 +1,79 @@
 <template>
-  <div>
+  <div id="returnInquire">
     <Header :txt="txt" :text="text" :isShow="true" :turn="turn" :isTrue="true"></Header>
-    <div>
-      <span >申请时间</span>
-      <cube-select v-model="value1" :options="options1"></cube-select>
-      <span>到</span>
-      <cube-select v-model="value2"></cube-select>
-      <cube-form>
-        <cube-button @click="showDatePicker">选择时间{{model.dateValue}}</cube-button>
-        <p @select="dateSelectHandler">11</p>
-      </cube-form>
+    <div class="box">
+      <div>
+        <cube-form>
+          <span @click="showDatePicker1">申请时间</span>
+          <p @click="dateSelectHandler1">{{dateValue1 || value}}</p>
+        </cube-form>
+      </div>
+      <div>
+        <cube-form>
+          <span @click="showDatePicker2">到</span>
+          <p @select="dateSelectHandler2">{{dateValue2 || value}}</p>
+        </cube-form>
+      </div>
+    </div>
+    <Scan></Scan>
+    <div class="order_detail">
+      <div class="title flex_box">
+        <h3>订单号: </h3>
+      </div>
+      <div class="flex_box">
+        <div>
+          <span class="span1">订单金额：</span>
+          <span>¥{{}}</span>
+        </div>
+        <div>
+          <span class="span1">支付方式：</span>
+          <span>{{}}</span>
+        </div>
+      </div>
+      <div class="flex_box">
+        <div>
+          <span class="span1">下单时间：</span>
+          <span>{{}}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { formatTime } from '@/js/time.js'
+import Scan from './scan'
 export default {
   name: 'ReturnInquire',
+  components: {
+    Scan
+  },
   data () {
     return {
       txt: '退货查询',
       text: '返回',
       turn: '/work',
-      value1: '123',
-      value2: '12',
-      options1: [1],
-      model: {
-        dateValue: ''
-      }
+      value: '',
+      dateValue1: '',
+      dateValue2: ''
     }
   },
   methods: {
-    showDatePicker () {
+    showDatePicker1 () {
+      if (!this.datePicker1) {
+        this.datePicker1 = this.$createDatePicker({
+          title: 'Date Picker',
+          min: new Date(2008, 7, 8),
+          max: new Date(2020, 9, 20),
+          value: new Date(),
+          columnOrder: [ 'year', 'month', 'date' ],
+          onSelect: this.dateSelectHandler1,
+          onCancel: this.cancelHandle
+        })
+      }
+      this.datePicker1.show()
+    },
+    showDatePicker2 () {
       if (!this.datePicker) {
         this.datePicker = this.$createDatePicker({
           title: 'Date Picker',
@@ -39,11 +81,10 @@ export default {
           max: new Date(2020, 9, 20),
           value: new Date(),
           columnOrder: [ 'year', 'month', 'date' ],
-          onSelect: this.dateSelectHandler,
+          onSelect: this.dateSelectHandler2,
           onCancel: this.cancelHandle
         })
       }
-
       this.datePicker.show()
     },
     // selectHandle (date, selectedVal, selectedText) {
@@ -60,14 +101,44 @@ export default {
         time: 1000
       }).show()
     },
-    dateSelectHandler (selectedVal) {
-      this.model.dateValue = new Date(selectedVal).toString()
-      console.log(`${selectedVal}`)
+    dateSelectHandler1 (selectedVal) {
+      // this.model.dateValue = new Date(selectedVal).toString()
+      // const str = this.model.dateValue.split()
+      // console.log(str)
+      // console.log(`${selectedVal}`)
+      // console.log(this.model.dateValue)
+      this.dateValue1 = formatTime(selectedVal)
+    },
+    dateSelectHandler2 (selectedVal) {
+      this.dateValue2 = formatTime(selectedVal)
     }
+  },
+  mounted () {
+    this.value = formatTime(new Date())
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  @import '~@/style/manager.scss';
+#returnInquire {
+  .box {
+    display: flex;
+    align-items: center;
+    height: 1.4rem;
+    justify-content: space-around;
+    color: #AFAFB3;
+    font-size: .4rem;
+    border-bottom: 1px solid #F2F2F2;
+    div {
+      .cube-form {
+        display: flex;
+        span {
+          color: #323233;
+          margin-right: .2rem;
+        }
+      }
+    }
+  }
+}
 </style>
